700c30db3ab896e6ced86a186859f31dfb05b53e
kaviyarasumaran <kaviyarasumaran@gmail.com>
2024-08-06 22:07:39 +0530

    adding app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/landing/Hero';
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Hero />
+    </>
+  );
+};
+
+export default App;
