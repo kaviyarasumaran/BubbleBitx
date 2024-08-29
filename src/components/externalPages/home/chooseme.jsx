620154006165b093a177367fc203c26e3e5fefd0
mytribe <kaviyarasumaran@gmail.com>
2024-08-29 11:28:42 +0530

    add

--- src/components/externalPages/home/chooseme.jsx
+++ src/components/externalPages/home/chooseme.jsx
@@ -26,6 +26,7 @@ const ChooseMe = () => {
                     alt="About Us"
                     boxSize="100%"
                     objectFit="cover"
+                    width={500}
                 />
             </Box>
 
@@ -34,14 +35,14 @@ const ChooseMe = () => {
             <Box w={['100%', '100%', '50%']}>
                 <VStack align="start" spacing={4}>
                     <HStack>
-                        <Text fontSize="xl" color="blue.600">
+                        <Text fontSize="m" color="blue.600" fontWeight="medium">
                             &#8505; ABOUT US
                         </Text>
                     </HStack>
-                    <Heading as="h2" size="lg" color="blue.900">
+                    <Text fontSize="lg" color="blue.900" textDecor="underline">
                         FEW REASONS WHY YOU SHOULD CHOOSE US
-                    </Heading>
-                    <Text color="gray.600">
+                    </Text>
+                    <Text color="gray.600" maxW={600} textAlign="justify" >
                         We are a young talented experience team of people, where we take
                         any work passionately. Ping us any time here to make your solutions
                         and businesses stand out and hand over better services to you with
@@ -50,19 +51,27 @@ const ChooseMe = () => {
 
                     <Flex spacing={4} mt={6} flexDirection={{ base: "column", sm: "row", }}>
                         <Box
-                            p={4}
-                            bgGradient="linear(to-r, teal.200, blue.400)"
-                            borderRadius="md"
-                            shadow="md"
-                            w="200px"
+                            bgGradient="radial-gradient(circle, rgba(37,90,66,1) 0%, rgba(17,17,17,1) 35%)"
+                            boxShadow="2xl"
+                            borderColor="transparent"
+                            borderRight="10px solid #44b17f"
+                            borderBottom="10px solid #44b17f"
                         >
-                            <Text fontSize="lg" fontWeight="bold" color="blue.900">
-                                Business growth
-                            </Text>
-                            <Text fontSize="sm" color="gray.700">
-                                We are passionate about our work. Our of the curve to engaging &
-                                user-friendly website to make it easy.
-                            </Text>
+                            <Box
+                                p={4}
+                                bgGradient="linear(to-r, teal.200, blue.400)"
+                                borderRadius="md"
+                                shadow="md"
+                                w="200px"
+                            >
+                                <Text fontSize="lg" fontWeight="bold" color="blue.900">
+                                    Business growth
+                                </Text>
+                                <Text fontSize="sm" color="gray.700">
+                                    We are passionate about our work. Our of the curve to engaging &
+                                    user-friendly website to make it easy.
+                                </Text>
+                            </Box>
                         </Box>
                         <Box
                             p={4}
